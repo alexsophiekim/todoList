4c99385e238d87760302b74eb3b6669743ae2992
Sophie <48259684+alexsophiekim@users.noreply.github.com>
2019-10-04 11:55:52 +1300

    recab

---
+++ js/script.js
@@ -0,0 +1,19 @@
+// Create a site for a shopping list, when you click on a button you will be prompt to input a value.
+// That value should then appear inside of a list on your html.
+// It should also get added to an array which gets consoled every time you click the button as well
+
+var items = [];
+
+function addToList(){
+  console.log("adding to list");
+  var newItem = document.getElementById("value").value ;
+
+  if (newItem.length >0){
+    document.getElementById("list").innerHTML += "<li>" + newItem + "</li>";
+    items.push(newItem);
+    newItem.clear();
+    console.log(items);
+  } else {
+    console.log("Please make sure input a value");
+  }
+}
